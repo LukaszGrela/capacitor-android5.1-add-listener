a3e33bf6c47dc86b91d63333970bff5d40463657
Lukasz Grela <lukasz.grela@gmail.com>
2019-09-06 13:51:44 +0100

    Add networkStatusChange listener

--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { Plugins } from '@capacitor/core';
+
+const { Network } = Plugins;
+
+let handler = Network.addListener('networkStatusChange', status => {
+  console.log('Network status changed', status);
+});
+// To stop listening:
+// handler.remove();
+
+// Get the current network status
+Network.getStatus().then(status => {
+  console.log('Network.getStatus()', status);
+});
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
